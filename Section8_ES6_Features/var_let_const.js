fa43e2d0a1a6cf9b8a94c1e33bdddcb640e3b3f1
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-07-11 17:02:02 +0530

    Create var_let_const.js

---
+++ Section8_ES6_Features/var_let_const.js
@@ -0,0 +1,80 @@
+// var employeeName = "Rahul";
+// console.log(employeeName);
+
+// function employeeDetails() {
+// 	if (true) {
+// 		var employeeName = "Rahul";
+// 	}
+// 	console.log(employeeName);
+// }
+
+// employeeDetails();
+
+// if (true) {
+// 	var department = "Engineering";
+// }
+
+// console.log(department);
+
+// function employeeDetails() {
+// 	if (true) {
+// 		let employeeName = "Rahul";
+// 	}
+// 	console.log(employeeName);
+// }
+
+// employeeDetails();
+
+// Redeclration
+// let department = "Engineering";
+
+// let department = "HR";
+
+// console.log(department);
+// var department = "Engineering";
+
+// var department = "HR";
+
+// console.log(department);
+
+// Reassignment
+// var department = "Engineering";
+
+// department = "HR";
+
+// console.log(department);
+
+// let department = "Engineering";
+
+// department = "HR";
+
+// console.log(department);
+
+// const employeeId = 101;
+// const employeeId = 102;
+// console.log(employeeId);
+
+const emplyoee = {
+	id: 101,
+	name: "Rahul",
+	department: "Engineering",
+};
+emplyoee.name = "Rohit";
+
+// Type Error
+// emplyoee = {
+// 	id: 102,
+// 	name: "Rohit",
+// 	department: "HR",
+// };
+
+console.log(emplyoee);
+
+let employeeSalary = 50000;
+employeeSalary = 75000;
+const companyName = "XYZ";
+
+const employee = {
+	id: 101,
+	name: "Rahul",
+};
